7f769b39c95408459790c0c2f3e50a2b4ac68f3c
son120 <truongdinhson3003@gmail.com>
2018-04-25 11:19:36 +0700

    Add BaiThucHanh1

---
+++ python/sohoc.py
@@ -0,0 +1,13 @@
+a = input("a = ")
+b = input("b = ")
+print 'Phuong trinh tong a va b'
+print '%d + %d = %d' %(a,b,a+b)
+
+print 'Phuong trinh hieu a va b'
+print '%d - %d = %d' %(a,b,a-b)
+
+print 'Phuong trinh tich a va b'
+print '%d * %d = %d' %(a,b,a*b)
+
+print 'Phuong trinh thuong a va b'
+print '%f / %f = %f' %(a,b,a/b)
---
+++ python/swap.py
@@ -0,0 +1,8 @@
+a = input('a = ')
+b = input('b = ')
+def swap(a,b):
+	return (b,a)
+a,b = swap(a,b)
+print 'Sau khi bien doi'
+print 'a = %d, b = %d' %(a,b)
+
