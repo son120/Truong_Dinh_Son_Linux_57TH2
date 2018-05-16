4c57552f36d5f9881ac638fee503df9e7ce83dc5
son120 <truongdinhson3003@gmail.com>
2018-05-16 08:43:40 +0700

    upload bai tap

---
+++ Thuc_Hanh_Lap_Trinh_Shell/Bai1.sh
@@ -0,0 +1,9 @@
+echo "***********************************"
+echo "Today is: ";date
+echo "Hello "; $LOGNAME
+echo "Your current working directory: "; $PWD
+echo "Your home directory: ";$HOME
+echo "Please press any key to finish: "
+read -p
+echo "Thanhk you very much!!"
+echo "***********************************"
